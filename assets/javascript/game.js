ae5ba383edff5095c48a7985608fe114c07e30d1
Austin Graves <austingraves106@aol.com>
2019-08-02 13:44:41 -0500

    Working on the flow of things through the javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,55 @@
-console.log('Hello World');
+const lettersGuessed = document.getElementById('letters-guessed');
+const chosenWord = document.getElementById('chosen-word');
+const startGame = document.getElementById('start-game');
+const restartGame = document.getElementById('restart-game');
+let userGuesses = [];
+let pickedWord;
+
+let guessableWords = ['austin', 'joel', 'sam'];
+
+// This is the word that the computer chooses for the user to guess
+
+window.onload = function() {
+  pickedWord = guessableWords[Math.floor(Math.random() * guessableWords.length)];
+  console.log(pickedWord);
+  for (var i = 0; i < pickedWord.length; i++) {
+    console.log(pickedWord[i]);
+    chosenWord.textContent += "_ "
+  }
+}
+ startGame.addEventListener('click', () => {
+    document.onkeyup = function() {
+    console.log('Game Started');
+    userGuesses.push(event.key);
+    for (var i = 0; i < userGuesses.length; i++) {
+      console.log(userGuesses[i]);
+      lettersGuessed.textContent = `${userGuesses} `;
+    } 
+    for (var i = 0; i < chosenWord.length; i++) {
+      for (var j = 0; j < userGuesses.length; j++) {
+        if (pickedWord[i] === userGuesses[j]) {
+          chosenWord[i].textContent ="";
+          chosenWord.textContent += userGuesses[j];
+        } else {
+        } 
+      }
+    }
+  }
+ });
+
+ restartGame.addEventListener('click', () => {
+   document.onkeyup = false;
+   lettersGuessed.textContent = "";
+  userGuesses = [];
+ });
+
+
+ for (var i = 0; i < pickedWord.length; i++) {
+   for (var j = 0; j < userGuesses.length; j++) {
+     if (pickedWord[i] === userGuesses[j]) {
+       chosenWord.textContent = userGuesses[j]
+     } else {
+
+     }
+   }
+ }
