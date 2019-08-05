e91da5c7262bc6b06c79df7bcc1366e7bc5079d5
Austin Graves <austingraves106@aol.com>
2019-08-05 09:38:21 -0500

    Gonna Restart from the ground up. Rethinking the approach to the challenge.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,6 @@ const startGame = document.getElementById('start-game');
 const restartGame = document.getElementById('restart-game');
 let userGuesses = [];
 let pickedWord;
-
 let guessableWords = ['austin', 'joel', 'sam'];
 
 // This is the word that the computer chooses for the user to guess
@@ -13,43 +12,16 @@ window.onload = function() {
   pickedWord = guessableWords[Math.floor(Math.random() * guessableWords.length)];
   console.log(pickedWord);
   for (var i = 0; i < pickedWord.length; i++) {
-    console.log(pickedWord[i]);
     chosenWord.textContent += "_ "
+    pickedWordLetters.push(pickedWord[i])
+    console.log(pickedWordLetters[i]);
   }
 }
  startGame.addEventListener('click', () => {
-    document.onkeyup = function() {
-    console.log('Game Started');
-    userGuesses.push(event.key);
-    for (var i = 0; i < userGuesses.length; i++) {
-      console.log(userGuesses[i]);
-      lettersGuessed.textContent = `${userGuesses} `;
-    } 
-    for (var i = 0; i < chosenWord.length; i++) {
-      for (var j = 0; j < userGuesses.length; j++) {
-        if (pickedWord[i] === userGuesses[j]) {
-          chosenWord[i].textContent ="";
-          chosenWord.textContent += userGuesses[j];
-        } else {
-        } 
-      }
-    }
-  }
+   
  });
 
  restartGame.addEventListener('click', () => {
-   document.onkeyup = false;
-   lettersGuessed.textContent = "";
-  userGuesses = [];
+
  });
 
-
- for (var i = 0; i < pickedWord.length; i++) {
-   for (var j = 0; j < userGuesses.length; j++) {
-     if (pickedWord[i] === userGuesses[j]) {
-       chosenWord.textContent = userGuesses[j]
-     } else {
-
-     }
-   }
- }
