a4ad12c27c3a00048d25ce8b59e93b70f55d8dd3
Austin Graves <austingraves106@aol.com>
2019-08-09 13:06:17 -0500

    Attempted to clean up some of the code and continue to work towards my issue with displaying the words. Still not there yet.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,69 @@
+//  --------------------- DOM Links ---------------------------
 const lettersGuessed = document.getElementById('letters-guessed');
 const chosenWord = document.getElementById('chosen-word');
 const startGame = document.getElementById('start-game');
 const restartGame = document.getElementById('restart-game');
 const remainingGuesses = document.getElementById('remaining-guesses');
+// ---------------------------------------------------------------
+
+// ---------------------- Programmatic Variables -----------------
 let userGuesses = [];
 let pickedWord;
-let pickedWordListOfLetters = [];
 let guessableWords = ['austin', 'joel', 'sam'];
+// ---------------------------------------------------------------
 
-// This is the word that the computer chooses for the user to guess
-
-// Utilize the .includes() functionality for an array to check if something is inside the array
-
-window.onload = function() {
+function pickingTheWord() {
+  // This is the word that the computer chooses for the user to guess
   pickedWord = guessableWords[Math.floor(Math.random() * guessableWords.length)];
   console.log(pickedWord);
   for (var i = 0; i < pickedWord.length; i++) {
     chosenWord.textContent += "_ ";
-    // Taking the Picked Word and getting it into a list.
-    pickedWordListOfLetters.push(pickedWord[i]);
-    // End of List Conversion
-    remainingGuesses.textContent = 14;
-    console.log(pickedWordListOfLetters[i]);
   }
 }
- startGame.addEventListener('click', () => {
-   document.onkeyup = function(event) {
+
+function inputEvaluation() {
+  if (userGuesses.includes(input)) {
+    console.log('That Guess Was Already Submitted')
+  } else {
+    userGuesses.push(input);
+    for (var i = 0; i < userGuesses.length; i++) {
+      if (lettersGuessed.textContent.includes(userGuesses[i])) {
+      console.log('The array already has this value' +userGuesses[i]);
+    } else {
+      lettersGuessed.textContent += userGuesses[i];
+    }
+  }
+  // Evaluate if the user types an input in that matches the picked word. 
+  // for (var i = 0; i < pickedWord.length; i++) {
+  //   if (input === pickedWord[i]) {
+  //     console.log('Matched the picked word');
+  //     for (var j = 0; j < chosenWord.length; j++) {
+  //       chosenWord[i].textContent = input;
+  //     }
+  //     // pickedWord[i].textContent = input;
+  //     console.log(pickedWord);
+  //   } else {
+  //     console.log('Did not match the picked word');
+  //     chosenWord[i].textContent = '_ '
+  //   }
+  // }
+  console.log(userGuesses);
+}
+}
+
+  startGame.addEventListener('click', () => {
+    remainingGuesses.textContent = 14;
+    pickingTheWord();
+    document.onkeyup = function(event) {
     // Gathering Input and Lowercasing it
      input = event.key;
      input = input.toLowerCase();
      console.log(input);
-    // End of Gathering
-    if (userGuesses.includes(input)) {
-      console.log('That Guess Was Already Submitted')
-    } else {
-      userGuesses.push(input);
-      // Try and place the user guess in the right place.
-      for (var i = 0; i < pickedWordListOfLetters.length; i++) {
-        if (input === pickedWordListOfLetters[i]) {
-          chosenWord.textContent[i] = input;
-        }
-      }
-      //    
-      for (var i = 0; i < userGuesses.length; i++) {
-        if (lettersGuessed.textContent.includes(userGuesses[i])) {
-          console.log('Already Exists');
-        } else {
-          lettersGuessed.textContent += userGuesses[i];
-        }
-      };
+     inputEvaluation();   
     }
-   }
- });
+  });
 
  restartGame.addEventListener('click', () => {
-
+  for (var i = 0; i < userGuesses.length; i++) {
+  } 
  });
